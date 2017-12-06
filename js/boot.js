4fcb4269b32dc93da41347c379271e80ef207328
Zynpai <32636952+Zynpai@users.noreply.github.com>
2017-12-06 21:44:36 +0000

    Update boot.js

--- js/boot.js
+++ js/boot.js
@@ -1,8 +1,8 @@
 var bootState = {
 	preload: function() {
-		game.load.image('loading', 'img/Loading.png');
-		game.load.image('stairs', 'img/stairs.jpg');
-		game.load.image('block', 'img/block.jpg');
+		game.load.image('loading', 'https://zynpai.github.io/Year1ProjectPlay/img/Loading.png');
+		game.load.image('stairs', 'https://zynpai.github.io/Year1ProjectPlay/img/stairs.jpg');
+		game.load.image('block', 'https://zynpai.github.io/Year1ProjectPlay/img/block.jpg');
 		game.load.spritesheet('player', 'img/player.jpg', 52, 52);
 	},
 	
