fa0ff72f2216ed9dc8ed6924a273394436d1c4cb
Zynpai <32636952+Zynpai@users.noreply.github.com>
2017-12-08 12:55:34 +0000

    Update game.js

--- js/game.js
+++ js/game.js
@@ -319,12 +319,12 @@ function bossFire() {
 	blaser.reset(boss.x + 405, boss.y +300);
 
 	if (bossLives <= 10){
-	game.physics.arcade.moveToObject(blaser,player,300);
-	firingTimerB = game.time.now + 2000;
+	game.physics.arcade.moveToObject(blaser,player,500);
+	firingTimerB = game.time.now + 3000;
 	}
 	else{
     game.physics.arcade.moveToObject(blaser,player,400);
-	firingTimerB = game.time.now + 3000;
+	firingTimerB = game.time.now + 2000;
 	}
 }	
 
