7e39afdeccaa1f5243e64c45261728c4f0b0e01c
Zynpai <32636952+Zynpai@users.noreply.github.com>
2018-06-04 21:08:45 +0100

    Update gameT.js

--- js/gameT.js
+++ js/gameT.js
@@ -69,7 +69,7 @@ var gameStateT = {
 
 init: function() {
 	// Listen to space & enter keys
-	var keys = [Phaser.KeyCode.SPACEBAR];
+	var keys = [Phaser.KeyCode.G];
 	// Create Phaser.Key objects for listening to the state
 	phaserKeys = game.input.keyboard.addKeys(keys);
 	// Capture these keys to stop the browser from receiving this event
